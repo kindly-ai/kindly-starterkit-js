dd62fea5519be9e0767d666058804db4682d4e66
Gunnar Fornes <gunnarfornes@gmail.com>
2018-04-13 10:59:43 +0200

    :wrench: configure server

--- src/server.js
+++ src/server.js
@@ -1,10 +1,7 @@
 import Hapi from 'hapi';
 
 const port = process.env.PORT || 9292;
-
-const host = process.env.HOST || 'localhost';
-
-const server = process.env.NODE_ENVIRONMENT === 'production' ?
-  Hapi.server(process.env.PORT) : Hapi.server({ port, host });
+const host = process.env.HOST || '0.0.0.0';
+const server = Hapi.server({ port, host });
 
 export default server;
